bcb1d1f45b4b92be646db3e3736e537189576897
Rémi Staelen <remistperso@gmail.com>
2023-02-03 16:35:15 +0100

    locust battle

--- app/tests/locustfile.py
+++ app/tests/locustfile.py
@@ -1,9 +1,14 @@
 from locust import HttpUser, task, between
 
+
 class QuickstartUser(HttpUser):
     wait_time = between(1, 5)
 
     @task
     def view_pokemons(self):
-        for pokemon in range(20):
-            self.client.get("/battle/" + str(pokemon) + "/" + str(pokemon + 1))
+        for pokemon in range(1, 20):
+            self.client.get("/pokemons/battle/" + str(pokemon) + "/" + str(pokemon + 1))
+
+    @task
+    def view_stats(self):
+        self.client.get("/pokemon/")
